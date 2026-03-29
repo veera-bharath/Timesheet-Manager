6d8f8283c56876d620fdec8cc5077d6633b09b03
veerabharath123 <veerab1029@gmail.com>
2026-03-29 18:59:30 +0530

    chore: bump version to 2.0.0

--- src/renderer/modules/state.js
+++ src/renderer/modules/state.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.4.0';
+export const APP_VERSION = '2.0.0';
 
 export const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 export const ROMAN = ['i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x',
